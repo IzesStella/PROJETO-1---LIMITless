d8967c2ebb58dfa87d5ac118c07d967b849b080f
Izes Stella <isbb@discente.ifpe.edu.br>
2024-08-24 03:32:55 -0300

    Tela do Sobre nós, html e começo do css deus nos ajude

--- php/sobrenos.php
+++ php/sobrenos.php
@@ -3,25 +3,104 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> LIMITless </title>
-    <link rel="stylesheet" href="../css/cadastro.css">
+    <title>LIMITless</title>
+    <link rel="stylesheet" href="../css/sobrenos.css">
 </head>
 <body>
     <header>
         <div class="logo">
             <img src="logo barra.png">
         </div>
-       
-        <nav>
-            <ul>
-                </li><a href="/php/sobrenos.php">Sobre Nós</a></li>
-                </li><a href="/php/entrar.php">Entrar </a></li> 
-                </li><a href="/php/cadastro.php">Cadastre-se</a></li> 
-            </ul>
-        </nav>    
-    </header>    
-    
+        <div class="logo">
+            <img src="../php/img/logo barra.png">
+        </div>
 
-    
+        <form class="search-form"> 
+            <input type="text" placeholder="Procurar itens...">
+        </form>
+
+        <nav>
+        <ul>
+                <?php if (isset($_SESSION['username_id'])): ?>
+                    <li><a href="#">Chat</a></li>
+                    <li><a href="#">Perfil</a></li> 
+                    <li><a href="/php/logout.php">Logout (<?= $_SESSION['user'] ?>)</a></li>
+                <?php else: ?>
+                    <li><a href="/php/sobrenos.php">Sobre Nós</a></li>
+                    <li><a href="/php/entrar.php">Entrar</a></li> 
+                    <li><a href="/php/cadastro.php">Cadastre-se</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="desenvolvedores">
+        <div class="card">
+            <img src="dyvamia.jpg" alt="IzesStella">
+            <div class="card-content">
+                <h2>Izes Linda</h2>
+                <div class="link">
+                    <a href="https://www.instagram.com/izesstella/">
+                        <img src="instalogo.png" alt="logoinsta" class="insta-logo">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="dyvamia.jpg" alt="Mari">
+            <div class="card-content">
+                <h2>Mariana Tavares</h2>
+                <div class="link">
+                    <a href="https://github.com/marianantavares">
+                        <img id="git" src={giticon} alt="GitHub">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="dyvamia.jpg" alt="Lucas">
+            <div class="card-content">
+                <h2>Lucas Amaro</h2>
+                <div class="link">
+                    <a href="https://github.com/lucazle">
+                        <img id="git" src={giticon} alt="GitHub">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="dyvamia.jpg" alt="Isabelly">
+            <div class="card-content">
+                <h2>Isabelly Arruda</h2>
+                <div class="link">
+                    <a href="https://github.com/IsabellyArrudaa">
+                        <img id="git" src={giticon} alt="GitHub">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="dyvamia.jpg" alt="Talita">
+            <div class="card-content">
+                <h2>Talita Vitória</h2>
+                <div class="social-link">
+                    <a href="https://github.com/Talitavit">
+                        <img id="git" src={giticon} alt="GitHub">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="button-container">
+    <a href="https://github.com/IzesStella/PROJETO-1---LIMITless" class="btn1">
+        <img src="githubicon.png" alt="Voltar" class="button-image">
+    </a>
+</div>
+
 </body>
 </html>
